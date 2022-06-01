76f9df33d972f17ada59ef3b76a3e15db64c9d9d
Thanh Nguyen <chrisng9@outlook.com>
2022-05-31 20:27:42 -0700

    Added ch7 interfip app

---
+++ ch7/p1/interfaces.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	// Get all network interfaces
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, interf := range interfaces {
+		addr, err := interf.Addrs()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("interface", interf.Name)
+		for _, add := range addr {
+			if ip, ok := add.(*net.IPNet); ok {
+				fmt.Printf("\t%v\n", ip)
+			}
+		}
+	}
+}
