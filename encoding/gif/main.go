37a9a2adc36abee36bae240d0b86fb1ad0902880
Thanh Nguyen <chrisng9@outlook.com>
2022-06-25 01:40:17 -0700

    Adding encoding csv, gif, and gob, example code

--- encoding/gif/main.go
+++ encoding/gif/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/binary"
+	"log"
+	"os"
+)
+
 type Version [6]byte
 
 func (v Version) String() string {
@@ -19,4 +25,16 @@ func (d Dimensions) Height() int {
 type GifHeader struct {
 	Version    Version
 	Dimensions Dimensions
+}
+
+func main() {
+	file, err := os.Open("test.gif")
+	if err != nil {
+		log.Fatalf("failed opening gif: %s", err)
+	}
+	defer file.Close()
+	var header GifHeader
+	binary.Read(file, binary.LittleEndian, &header)
+	log.Printf("decoded a %s with width %dpx and height %dpx", header.Version, header.Dimensions.Width(),
+		header.Dimensions.Height())
 }
